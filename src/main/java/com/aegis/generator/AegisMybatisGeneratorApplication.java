9c85539755fcb38878013421461104f31c287beb
Lambert <lambert@gmail.com>
2024-12-13 20:40:05 +0800

    feat: 初始化 aegis-mybatis-generator 项目
    - 创建 Spring Boot应用程序主类和测试类
    - 添加 Maven 构建配置文件 pom.xml
    - 设置 .gitattributes 和 .gitignore 文件
    - 添加 Maven Wrapper 配置和脚本文件
    - 创建应用程序属性文件

---
+++ src/main/java/com/aegis/generator/AegisMybatisGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.aegis.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AegisMybatisGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AegisMybatisGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aegis/generator/AegisMybatisGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aegis.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AegisMybatisGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
